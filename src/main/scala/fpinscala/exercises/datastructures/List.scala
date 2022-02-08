b5c2d8f190afc18101969038ac3a5f494d80ab58
tocoteron <tocoteron@gmail.com>
2022-02-08 22:43:42 +0900

    Implement dropWhile

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -63,7 +63,11 @@ object List: // `List` companion object. Contains functions for creating and wor
       case Nil => Nil
       case Cons(_, xs) => drop(xs, n - 1)
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
+    def loop(l: List[A]): List[A] = l match 
+      case Nil => Nil
+      case Cons(x, xs) => if (f(x)) loop(xs) else l
+    loop(l)
 
   def init[A](l: List[A]): List[A] = ???
 
