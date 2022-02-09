6c220b4483a67d4b3b826ff3c655a81d35bdce7e
tocoteron <tocoteron@gmail.com>
2022-02-09 23:53:35 +0900

    Implement length

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -83,7 +83,8 @@ object List: // `List` companion object. Contains functions for creating and wor
     loop(l, Nil)
   }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0, (_, acc) => acc + 1)
 
   def foldLeft[A,B](l: List[A], acc: B, f: (B, A) => B): B = ???
 
