280c33eb679cde2db0f06552bf896b5bceec3d85
tocoteron <tocoteron@gmail.com>
2022-02-11 00:11:58 +0900

    Implement doubleToString

--- src/main/scala/fpinscala/exercises/datastructures/List.scala
+++ src/main/scala/fpinscala/exercises/datastructures/List.scala
@@ -114,7 +114,8 @@ object List: // `List` companion object. Contains functions for creating and wor
   def incrementEach(l: List[Int]): List[Int] =
     foldRight(l, Nil: List[Int], (x, xs) => Cons(x + 1, xs))
 
-  def doubleToString(l: List[Double]): List[String] = ???
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String], (x, xs) => Cons(x.toString, xs))
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 
