e100a353009383341d0495c92e48e20c516d6044
tocoteron <tocoteron@gmail.com>
2022-02-11 22:12:49 +0900

    Implement getOrElse

--- src/main/scala/fpinscala/exercises/errorhandling/Option.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Option.scala
@@ -11,7 +11,9 @@ enum Option[+A]:
     case None => None
     case Some(x) => Some(f(x))
 
-  def getOrElse[B>:A](default: => B): B = ???
+  def getOrElse[B>:A](default: => B): B = this match
+    case None => default
+    case Some(x) => x
 
   def flatMap[B](f: A => Option[B]): Option[B] = ???
 
