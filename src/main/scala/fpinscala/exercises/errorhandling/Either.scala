a72987d90a51265de4abae773a46d7182fdb2120
tocoteron <tocoteron@gmail.com>
2022-02-13 08:03:44 +0900

    Implement orElse

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -16,7 +16,9 @@ enum Either[+E,+A]:
     case Left(e) => Left(e)
     case Right(a) => f(a)
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match
+    case Left(e) => b
+    case Right(a) => Right(a)
 
   def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
 
